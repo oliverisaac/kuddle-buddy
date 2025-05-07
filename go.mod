5ad823c8a8b0ce10021f433841d14ad2d04568f2
Oliver Isaac <oisaac@gmail.com>
2025-05-06 23:03:03 -0500

    initial flag parsing
    Signed-off-by: Oliver Isaac <oisaac@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/oliverisaac/kuddle-buddy
 
 go 1.24
 
-require github.com/pkg/errors v0.9.1
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/pflag v1.0.6
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,70 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
 )
 
 func main() {
 	kuddleArgs := applyTweaksToArgs(os.Args[1:])
 
-	exitCode, err := runAttachedCommand("kubectl", kuddleArgs...)
+	var exitCode int
+	var err error
+	if commandOrSubcommand(os.Args, "kcontainers") {
+		exitCode, err = kcontainers(kuddleArgs)
+	} else {
+		exitCode, err = runAttachedCommand("kubectl", kuddleArgs...)
+	}
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "Failed to run the command"))
 		os.Exit(1)
 	}
 	os.Exit(exitCode)
+}
+
+func commandOrSubcommand(osArgs []string, needle string) bool {
+	if osArgs[0] == needle {
+		return true
+	}
+	if len(osArgs) > 1 && osArgs[1] == needle {
+		return true
+	}
+	return false
+}
+
+func kcontainers(args []string) (int, error) {
+	flags := pflag.NewFlagSet("kcontainers", pflag.ExitOnError)
+
+	var context string
+	flags.StringVarP(&context, "context", "c", "", "Context to get contianers in")
+
+	var namespace string
+	flags.StringVarP(&namespace, "namespace", "n", "", "Namespace to get contianers in")
+
+	var allNamespaces bool
+	flags.BoolVarP(&allNamespaces, "all-namespaces", "A", false, "Get containers in all namespaces")
+
+	err := flags.Parse(args)
+	if err != nil {
+		return 1, errors.Wrap(err, "parsing flags")
+	}
+
+	logrus := logrus.WithFields(logrus.Fields{
+		"context":        context,
+		"namespace":      namespace,
+		"all-namespaces": allNamespaces,
+	})
+
+	logrus.Info("Going to run kcontainers")
+
+	return 0, fmt.Errorf("not impl")
 }
 
 // applyTweaksToArgs modifies the args sent in so they work wtih kubectl
